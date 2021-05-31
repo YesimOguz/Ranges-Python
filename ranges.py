2c2d09bef5726146699129e837461bae885ab7c7
YesimOguz <yessoguzzz@gmail.com>
2021-05-31 23:30:28 +0300

    first commit

---
+++ ranges.py
@@ -0,0 +1,11 @@
+# this program generate a list of numbers starting from 2 to 79 with a step of 3
+lst = []
+for x in range(2,80,3):
+    lst.append(x)
+print(lst)
+
+# this program counts backwards from 100 to 0 with a step of 7
+numbers = []
+for y in range(100,0,-7):
+    numbers.append(y)
+print(numbers)
